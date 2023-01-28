626265bdf0264bbc56b3fae26152d0f875161f86
ParkKiHun <miy1205@naver.com>
2023-01-28 14:48:27 +0900

    과제 2: 이벤트 & 상태 작업하기

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,16 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const changeYearHandler = (year) => {
+    console.log(year);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter onChangeYear={changeYearHandler} />
       {props.expenses.map((item, index) => (
         <ExpenseItem
           key={index}
