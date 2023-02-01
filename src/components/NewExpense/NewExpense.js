4318997345e9ac7b662b45f3e01b1c7d427794f0
ParkKiHun <miy1205@naver.com>
2023-02-01 20:31:18 +0900

    76.연습하기 조건부 내용 (정답)

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enterExpenseData) => {
     const expenseData = {
       id: Math.random().toString(),
       ...enterExpenseData,
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
   };
 
-  return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
-  );
+  const clickHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandle = () => {
+    setIsEditing(false);
+  };
+
+  let expenseContents = <button onClick={clickHandler}>New Add Expense</button>;
+
+  if (isEditing) {
+    expenseContents = (
+      <ExpenseForm
+        onSaveExpenseData={saveExpenseDataHandler}
+        onCancel={stopEditingHandle}
+      />
+    );
+  }
+
+  return <div className="new-expense">{expenseContents}</div>;
 };
 
 export default NewExpense;
